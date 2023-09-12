de61369475cee8bce0782d8f2825d4b1b1e5377b
mateusmgonza <111081316+mateusmgonza@users.noreply.github.com>
2023-09-12 10:46:34 -0300

    pl work

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ function TocaSomToim (){
 }
 
 const ListaDeSons = document.querySelectorAll('.tecla');
+
+let Contador = 0;
+
+while (Contador <9){
+    ListaDeSons[5].onclick = TocaSomToim;
+    Contador = Contador + 1;
+    console.log(Contador);
+}
