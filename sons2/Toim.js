0c0dfe39da91acc7e303ea2a1204d99d8174f2bb
mateusmgonza <111081316+mateusmgonza@users.noreply.github.com>
2023-09-19 10:35:04 -0300

    ghsg

--- sons2/Toim.js
+++ sons2/Toim.js
@@ -16,7 +16,9 @@ while (Contador <9){
     tocar('#som_tecla_toim');
     }
     const classe = ListaDeSons[Contador].classList[1];
+    console.log(classe);
     //acessei a lista - peguei o elemento e pedi para tocar quando clicado
     Contador = Contador + 1;
+    console.log(Contador);
     // mudando o valor da variável - somei 1 
 }
