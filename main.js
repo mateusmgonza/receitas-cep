e314856cde07d465148bffa48416d0f467cb5d1a
Arataki <107219123+aratakikuun@users.noreply.github.com>
2023-09-19 09:08:41 -0300

    .

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ while (Contador <9){
     Contador = Contador + 1;
     console.log(Contador);
 }
+
+function TocaSomPom (){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const ListaDeSons = document.querySelectorAll('.tecla');
+
+let Contador = 0;
+
+while (Contador <9){
+    ListaDeSons[5].onclick = TocaSomPom;
+    Contador = Contador + 1;
+    console.log(Contador);
+}
