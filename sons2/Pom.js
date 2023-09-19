3c943b882b0d502688bbad14ac6a5ac246f78374
Arataki <107219123+aratakikuun@users.noreply.github.com>
2023-09-19 09:18:51 -0300

    .

--- sons2/Pom.js
+++ sons2/Pom.js
@@ -7,7 +7,7 @@ const ListaDeSons = document.querySelectorAll('.tecla');
 let Contador = 0;
 
 while (Contador <9){
-    ListaDeSons[1].onclick = TocaSomPom;
+    ListaDeSons[0].onclick = TocaSomPom;
     Contador = Contador + 1;
     console.log(Contador);
 }
