56b54382ea5840e149575480896c106ec9aa09e8
Arataki <107219123+aratakikuun@users.noreply.github.com>
2023-09-19 09:32:01 -0300

    .

--- sons2/Toim.js
+++ sons2/Toim.js
@@ -1,13 +1,19 @@
+//função = tocar
 function TocaSomToim (){
+    //tocando Toim
     document.querySelector('#som_tecla_toim').play();
 }
 
+//constante = ListaDeSons - Teclas
 const ListaDeSons = document.querySelectorAll('.tecla');
 
+// variavel = contador = valor fixado 0
 let Contador = 0;
 
 while (Contador <9){
+    //acessei a lista - peguei o elemento e pedi para tocar quando clicado
     ListaDeSons[5].onclick = TocaSomToim;
     Contador = Contador + 1;
+    //console conversando
     console.log(Contador);
 }
