75af357ec84a1a0d234eae52e06fff2d74a7a5e7
Arataki <107219123+aratakikuun@users.noreply.github.com>
2023-09-19 09:16:20 -0300

    .

---
+++ sons2/Pom.js
@@ -0,0 +1,13 @@
+function TocaSomPom (){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const ListaDeSons = document.querySelectorAll('.tecla');
+
+let Contador = 0;
+
+while (Contador <9){
+    ListaDeSons[5].onclick = TocaSomPom;
+    Contador = Contador + 1;
+    console.log(Contador);
+}
