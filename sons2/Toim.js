e91b1e5bb790f6518af9c14fc07533d1ee7b3387
mateusmgonza <111081316+mateusmgonza@users.noreply.github.com>
2023-09-28 08:12:13 -0300

    56

--- sons2/Toim.js
+++ sons2/Toim.js
@@ -9,16 +9,34 @@ const ListaDeSons = document.querySelectorAll('.tecla');
 
 // variavel = contador = valor fixado 0
 let Contador = 0;
+while (contador < lista.lentgh){
+    const tecla = lista[Contador];
+    const classe = tecla.classList[1];
+    tecla.onclick = function (){
+        tocar(idSom);
+    }
 
+}
 // condição ou enquanto
 while (Contador <9){
+
     ListaDeSons[Contador].onclick = function (){
     TocaSomToim('#som_tecla_toim');
     }
-    const classe = ListaDeSons[Contador].classList[1];
-    console.log(classe);
+
+
+const classe = ListaDeSons[Contador].classList[1];
+console.log(classe);
     //acessei a lista - peguei o elemento e pedi para tocar quando clicado
-    Contador = Contador + 1;
-    console.log(Contador);
+const idSom = `#som_${classe}`;
+console.log(idSom);
+
+
+Contador = Contador + 1;
+console.log(Contador);
     // mudando o valor da variável - somei 1 
+
+    tecla.classList.add ('ativa');
+    tecla.classList.remove ('ativa')
+
 }
