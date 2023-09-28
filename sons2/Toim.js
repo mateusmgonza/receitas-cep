88cc8b4eb63c5508d057aef803770e332bc20f4a
mateusmgonza <111081316+mateusmgonza@users.noreply.github.com>
2023-09-28 08:50:16 -0300

    .

--- sons2/Toim.js
+++ sons2/Toim.js
@@ -13,7 +13,7 @@ let Contador = 0;
 while (Contador < ListaDeSons.length) {
     const tecla = ListaDeSons[Contador];
     const classe = tecla.classList[1];
-    const idSom = ;
+    const idSom = `#som_${classe}`;
     tecla.onclick = function () {
         TocaSomToim(idSom);
     }
